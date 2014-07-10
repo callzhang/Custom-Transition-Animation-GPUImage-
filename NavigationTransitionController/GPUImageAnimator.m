919a1aa379ee125ed9ace640dbbf1cd34e65344f
Lei Zhang <leizhang0121@gmail.com>
2014-07-10 00:23:21 -0400

    OK

--- NavigationTransitionController/GPUImageAnimator.m
+++ NavigationTransitionController/GPUImageAnimator.m
@@ -13,14 +13,14 @@
 #import "UIView+OBJSnapshot.h"
 #import "GPUImageView.h"
 
-static const float duration = 1;
+static const float duration = 0.3;
 
 @interface GPUImageAnimator ()
 
-@property (nonatomic, strong) GPUImageDissolveBlendFilter* blend;
+//@property (nonatomic, strong) GPUImageDissolveBlendFilter* blend;
 @property (nonatomic, strong) GPUImagePicture* sourceImage;
-@property (nonatomic, strong) GPUImageiOSBlurFilter* sourcePixellateFilter;
-@property (nonatomic, strong) GPUImagePicture* targetImage;
+@property (nonatomic, strong) GPUImageiOSBlurFilter* sourceBlurFilter;
+//@property (nonatomic, strong) GPUImagePicture* targetImage;
 //@property (nonatomic, strong) GPUImageiOSBlurFilter* targetPixellateFilter;
 @property (nonatomic, strong) GPUImageView* imageView;
 @property (nonatomic, strong) id <UIViewControllerContextTransitioning> context;
@@ -43,24 +43,26 @@ static const float duration = 1;
 - (void)setup
 {
     self.imageView = [[GPUImageView alloc] init];
-    self.imageView.alpha = 0;
+    //self.imageView.alpha = 0;
     self.imageView.opaque = NO;
     
-    self.blend = [[GPUImageDissolveBlendFilter alloc] init];
-    self.blend.mix = 0;
+//    self.blend = [[GPUImageDissolveBlendFilter alloc] init];
+//    self.blend.mix = 0;
+//    [self.blend addTarget:self.imageView];
     
-    self.sourcePixellateFilter = [[GPUImageiOSBlurFilter alloc] init];
-    self.sourcePixellateFilter.blurRadiusInPixels = 0;
+    
+    self.sourceBlurFilter = [[GPUImageiOSBlurFilter alloc] init];
+    self.sourceBlurFilter.blurRadiusInPixels = 0;
     
     //self.targetPixellateFilter = [[GPUImageiOSBlurFilter alloc] init];
     //self.targetPixellateFilter.blurRadiusInPixels = 20;
     
-    [self.sourcePixellateFilter addTarget:self.blend];
+    //[self.sourcePixellateFilter addTarget:self.blend];
     //[self.targetPixellateFilter addTarget:self.blend];
+    [self.sourceBlurFilter addTarget:self.imageView];
     
-    [self.blend addTarget:self.imageView];
     
-    self.displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(frame:)];
+    self.displayLink = [CADisplayLink displayLinkWithTarget:self selector:@selector(updateFrame:)];
     [self.displayLink addToRunLoop:[NSRunLoop mainRunLoop] forMode:NSRunLoopCommonModes];
     self.displayLink.paused = YES;
 }
@@ -76,26 +78,27 @@ static const float duration = 1;
     self.context = transitionContext;
     UIViewController* toViewController = [transitionContext viewControllerForKey:UITransitionContextToViewControllerKey];
     UIViewController* fromViewController = [transitionContext viewControllerForKey:UITransitionContextFromViewControllerKey];
-    
+    //toView
     UIView* toView = toViewController.view;
     UIView* fromView = fromViewController.view;
-    
     UIView* container = [transitionContext containerView];
     [container addSubview:toView];
-    [container sendSubviewToBack:toView];
+    toView.alpha = 0;
+    toView.transform = CGAffineTransformMakeScale(1.1, 1.1);
     
     self.imageView.frame = container.bounds;
     [container addSubview:self.imageView];
+    //[container sendSubviewToBack:self.imageView];
     
     self.sourceImage = [[GPUImagePicture alloc] initWithImage:fromView.objc_snapshot];
-    [self.sourceImage addTarget:self.sourcePixellateFilter];
+    [self.sourceImage addTarget:self.sourceBlurFilter];
     
-    self.targetImage = [[GPUImagePicture alloc] initWithImage:toView.objc_snapshot];
+    //self.targetImage = [[GPUImagePicture alloc] initWithImage:toView.objc_snapshot];
     //[self.targetImage addTarget:self.targetPixellateFilter];
     
     [self triggerRenderOfNextFrame];
     
-    toView.alpha = 0;
+    
     self.imageView.alpha = 1;
     self.startTime = 0;
     self.displayLink.paused = NO;
@@ -105,19 +108,18 @@ static const float duration = 1;
 - (void)triggerRenderOfNextFrame
 {
     [self.sourceImage processImage];
-    [self.targetImage processImage];
+    //[self.targetImage processImage];
 }
 
 - (void)startInteractiveTransition:(id <UIViewControllerContextTransitioning>)transitionContext {
     [self animateTransition:transitionContext];
 }
 
-- (void)frame:(CADisplayLink*)link
+- (void)updateFrame:(CADisplayLink*)link
 {
     [self updateProgress:link];
-    self.blend.mix = self.progress;
-    self.sourcePixellateFilter.blurRadiusInPixels = self.progress * 20;
-    //rself.targetPixellateFilter.blurRadiusInPixels = (1- self.progress) * 20;
+    //self.blend.mix = self.progress;
+    self.sourceBlurFilter.blurRadiusInPixels = self.progress * 10;
     [self triggerRenderOfNextFrame];
     
     if (self.progress == 1 && !self.interactive) {
@@ -125,6 +127,7 @@ static const float duration = 1;
     }
 }
 
+//update progress
 - (void)updateProgress:(CADisplayLink*)link
 {
     if (self.interactive) return;
@@ -146,13 +149,22 @@ static const float duration = 1;
 - (void)finishInteractiveTransition
 {
     self.displayLink.paused = YES;
-    [self.imageView removeFromSuperview];
-    [self.context viewControllerForKey:UITransitionContextFromViewControllerKey].view.alpha = 1;
-    [self.context viewControllerForKey:UITransitionContextToViewControllerKey].view.alpha = 1;
     if (self.interactive) {
         [self.context finishInteractiveTransition];
     }
-    [self.context completeTransition:YES];
+    
+    
+    //uiview
+    
+    UIView *toView = [self.context viewControllerForKey:UITransitionContextToViewControllerKey].view;
+    [self.context.containerView bringSubviewToFront:toView];
+    [UIView animateWithDuration:0.3 animations:^{
+        toView.alpha = 1;
+        toView.transform = CGAffineTransformIdentity;
+    }completion:^(BOOL finished) {
+        
+        [self.context completeTransition:YES];
+    }];
 }
 
 - (void)cancelInteractiveTransition
